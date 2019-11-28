6a9437e2c5ac79c7d985b379cc94ce4f3a8f62e8
noelle <noelle.kyla@gmail.com>
2019-11-28 14:01:25 +1300

    Made the navigation bar responsive Added the icon on title bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 @section('title', 'Al-Qurain Al-Ahlia')
 
 @section('content')
-    <h1>Hello World</h1>
+
 @endsection
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="shortsut icon" href="{{ asset('images/icon.png') }}"/>
     <title>@yield('title')</title>
 
 </head>
@@ -13,18 +14,22 @@
 <header>
     <nav>
     <div class="logo">
-        <img src="logo.png" alt="logo-image" class="logo-image">
+        <img src={{ asset('images/logo.png') }} alt="logo-image" class="logo-image">
     </div>
-            <ul class="nav-links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
+    <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
 
     </nav>
-    </div>
 </header>
 
 @yield('content')
