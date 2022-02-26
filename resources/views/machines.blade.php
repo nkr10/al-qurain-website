0b4c1d354cc2594f95eed10d12dde25438baae1d
noelle <noelle.kyla@gmail.com>
2022-02-27 01:51:17 +1300

    Made the Machines images responsive

--- resources/views/machines.blade.php
+++ resources/views/machines.blade.php
@@ -11,7 +11,7 @@
                 @foreach ($machines as $machine)
                     <div class="col-lg-4 mb-3">
                         <div class="card">
-                            <div class="image-container"><img src="img_vehicles/{{ $machine->image_path }}" class="card-img-top" alt="{{ $machine->name }}"></div>
+                            <div class="image-container"><img src="img_vehicles/{{ $machine->image_path }}" class="img-fluid card-img-top" alt="{{ $machine->name }}"></div>
 
                             <div class="card-body">
                                 <h5 class="card-title">{{ $machine->name }}</h5>
